0f6a9499390761e749711e612d1adf04e2864ed9
Edi6758 <ediarts7@gmail.com>
2023-08-16 12:41:18 -0300

    reduce example

---
+++ reduce/index.js
@@ -0,0 +1,58 @@
+const personList = [
+    {
+        name: "Joao",
+        age: 20,
+        gender: "male"
+
+    },
+    {
+        name: "Maria",
+        age: 25,
+        gender: "female"
+    },
+    {
+        name: "Pedro",
+        age: 30,
+        gender: "male"
+    },
+    {
+        name: "Ana",
+        age: 35,
+        gender: "female"
+    },
+    {
+        name: "Jose",
+        age: 40,
+        gender: "male"
+    },
+    {
+        name: "Joana",
+        age: 45,
+        gender: "female"
+    },
+    {
+        name: "Lucas",
+        age: 50,
+        gender: "male"
+    },
+    {
+        name: "Ana",
+        age: 55,
+        gender: "female"
+    }
+]
+
+
+const person = personList.reduce((acc, object) => {
+    return {
+        ...acc,
+        [object.name]: {
+            age: object.age
+        }
+    }
+})
+
+const ageFromPerson = personList.reduce((acc, object) => {
+    acc.push(object.age)
+    return acc
+})
